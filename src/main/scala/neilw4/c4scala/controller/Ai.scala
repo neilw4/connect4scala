390f0e7c3a548932217250fcf8fb66282af38319
Neil Wells <neilw4@googlemail.com>
2014-09-24 23:59:02 +0100

    AI is interruptible

--- src/main/scala/neilw4/c4scala/controller/Ai.scala
+++ src/main/scala/neilw4/c4scala/controller/Ai.scala
@@ -1,47 +1,59 @@
 package neilw4.c4scala.controller
 
 import neilw4.c4scala.state._
-import neilw4.c4scala.util.Maths
+import neilw4.c4scala.util.{SimpleAsyncTask, Maths}
 
 import scala.util.control.Breaks._
 
+/**
+ * AIs that run in the background should extend this class.
+ */
+abstract class AsyncAi(var board: Board, controller: Controller) extends SimpleAsyncTask[Int, Int] {
+    override def doInBackground(depth: Int): Int = adviseMove(depth)
+    override def onPostExecute(col: Int) = controller.makeMove(col)
 
-trait Ai {
-        def adviseMove(depth: Int): Int
+    /**
+     * Core of the AI's functionality should be in here.
+     * Returns the column number to move in.
+     */
+    def adviseMove(difficulty: Int): Int
 }
 
 object ScalaAi {
-  val TAG = this.getClass.toString
+    val TAG = this.getClass.toString
 
-  val WIN = Int.MaxValue
-  val LOSE = -WIN
-  val DRAW = 0
-  val NO_WIN = 0
+    val WIN_SCORE = Int.MaxValue
+    val LOSE_SCORE = -WIN_SCORE
+    val DRAW_SCORE = 0
 
-  // Eval function uses this
+  // Bound on how far the eval function should look.
   val MAX_MOVES_AWAY = 5
 }
 
-class ScalaAi(var board: Board) extends Ai {
-    board = board.clone()
+// Note that this clones the board to avoid disturbing the original.
+class ScalaAi(_board: Board, controller: Controller) extends AsyncAi(_board.clone, controller) {
 
-    def adviseMove(depth: Int) = negamax(depth, None, -Int.MaxValue, Int.MaxValue)._1
+    def adviseMove(difficulty: Int) = negamax(difficulty, None, -Int.MaxValue, Int.MaxValue)._1
 
     def negamax(depth: Int, lastCol: Option[Int], alpha: Int, beta: Int): (Int, Int) = {
+        if (isCancelled) {
+            // Stop immediately.
+            return (-1, -1)
+        }
         if (lastCol.isDefined) {
             val winner = checkWin(lastCol.get)
             if (winner.isDefined) {
                 val score = winner.get match {
-                    case BLANK => ScalaAi.DRAW // Draw is equally bad for both players.
-                    case _ => ScalaAi.LOSE // Losing is bad.
+                    case BLANK => ScalaAi.DRAW_SCORE // Draw is equally bad for both players.
+                    case _ => ScalaAi.LOSE_SCORE // Losing is bad.
                 }
-                println(s"$depth endgame score $score, (eval ${eval(board.nextPiece)})")
-                return (-1, score)
+                // We are guaranteed to lose, so just play a random valid column.
+                val bestCol = colsFromCentre.find(board.canAdd).getOrElse(-2)
+                return (bestCol, score)
             }
         }
 
         if (depth == 0) {
-            println(s"$depth leaf eval_score ${eval(board.nextPiece)}")
             return (-1, eval(board.nextPiece))
         }
 
@@ -51,44 +63,39 @@ class ScalaAi(var board: Board) extends Ai {
             if(board.add(col)) {
                 val score = -negamax(depth - 1, Some(col), -beta, -bestScore)._2
                 board.remove(col)
-                println(s"$depth col $col score $score best $bestScore")
                 if (score > bestScore) {
                     bestScore = score
                     bestCol = col
                     if (score >= beta) {
-                        println(s"$depth col $col BETA $beta score $score")
                         //TODO: fix alpha-beta
                         //return (bestCol, score)
                     }
                 }
             }
         }
-        println(s"$depth ALL col $bestCol score $bestScore")
-        return (bestCol, bestScore)
+        (bestCol, bestScore)
     }
 
+    // Search columns in this order, as the middle columns are
+    // often the best position, so they should be searched first.
     val colsFromCentre = List(3, 4, 2, 5, 1, 6, 0)
 
+    /** A sequence of four cells in a line, with their coordinates and values. */
     class Sequence(val col: Int, val row: Int, val deltaCol: Int, val deltaRow: Int) extends IndexedSeq[(Int, Int, Piece)] {
-
         override def apply(offset: Int) = {
             val newCol = col + offset * deltaCol
             val newRow = row + offset * deltaRow
             (newCol, newRow, board(newCol)(newRow))
         }
-
         val length = 4
     }
 
     class HorizontalSequence(col: Int, row: Int) extends Sequence(col, row, 1, 0) {}
-
     class VerticalSequence(col: Int, row: Int) extends Sequence(col, row, 0, 1) {}
-
     class Diagonal1Sequence(col: Int, row: Int) extends Sequence(col, row, 1, 1) {}
-
     class Diagonal2Sequence(col: Int, row: Int) extends Sequence(col, row, -1, 1) {}
 
-
+    /** Counts the number of pieces of a certain type in the sequence. */
     def getCount(sequence: Sequence, piece: Piece) = sequence.takeWhile(_._3 == piece).size
 
     private val horizontalSequences =
@@ -110,7 +117,10 @@ class ScalaAi(var board: Board) extends Ai {
     val allSequences =
         horizontalSequences ++ verticalSequences ++ diagonal1Sequences ++ diagonal2Sequences
 
-    //TODO: memoise
+    /** All sequences containing a certain cell. */
+    def sequencesContaining(col: Int, row: Int) =
+        horizontalSequencesContaining(col, row) ++ verticalSequencesContaining(col, row) ++ diagonal1SequencesContaining(col, row) ++ diagonal2SequencesContaining(col, row)
+
     private def horizontalSequencesContaining(col: Int, row: Int): IndexedSeq[Sequence] =
         for (offset <- Maths.max(-3, -col) to Maths.min(0, Board.WIDTH - 4 - col))
         yield new HorizontalSequence(col + offset, row)
@@ -127,26 +137,27 @@ class ScalaAi(var board: Board) extends Ai {
         for (offset <- Maths.max(-3, -row, 1 + col - Board.WIDTH) to Maths.min(0, col - 3, Board.HEIGHT - 4 - row))
         yield new Diagonal2Sequence(col - offset, row + offset)
 
-    def sequencesContaining(col: Int, row: Int) =
-        horizontalSequencesContaining(col, row) ++ verticalSequencesContaining(col, row) ++ diagonal1SequencesContaining(col, row) ++ diagonal2SequencesContaining(col, row)
-
-
-    def checkWin(col: Int): Option[Piece] = {
+    /**
+     * Checks to see if the game has ended.
+     * @param lastCol the last column used.
+     * @return None if the game has ended, BLANK for a draw, or the winning piece.
+     */
+    def checkWin(lastCol: Int): Option[Piece] = {
         if (board.isFull) {
             return Some(BLANK)
         }
-        val row = board.heights(col) - 1
-        val piece = board(col)(row)
+        val row = board.heights(lastCol) - 1
+        val piece = board(lastCol)(row)
 
-        for (sequence <- sequencesContaining(col, row)) {
+        for (sequence <- sequencesContaining(lastCol, row)) {
             if (sequence.forall(_._3 == piece)) {
                 return Some(piece)
             }
         }
-        return None
+        None
     }
 
-    // Returns the value of the board from the viewpoint of a piece.
+    /** @return the value of the board from the viewpoint of a piece. */
     def eval(viewPoint: Piece): Int = {
         var totalScore: Int = 0
 
